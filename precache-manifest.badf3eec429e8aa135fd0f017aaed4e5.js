6e1eed543c6a7158985182ffbde60381830c1e31
Tony Martin <tony.venom007@gmail.com>
2020-03-28 16:59:29 +0530

    Updates

---
+++ precache-manifest.badf3eec429e8aa135fd0f017aaed4e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04b70a9a1a4b8dca07a8fa53861a1eb",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "89e7ca78d016e5ba43e5",
+    "url": "/covid19/static/css/main.c625023e.chunk.css"
+  },
+  {
+    "revision": "4d1c8a59f42630db1583",
+    "url": "/covid19/static/js/2.1b9b8c9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.1b9b8c9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e7ca78d016e5ba43e5",
+    "url": "/covid19/static/js/main.fc4ce4b5.chunk.js"
+  },
+  {
+    "revision": "2897f32d4e7477ba260b",
+    "url": "/covid19/static/js/runtime-main.a021e852.js"
+  }
+]);
